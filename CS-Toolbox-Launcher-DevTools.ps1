9a4e9ed3d6b220af7a2e52c8e4e45efc2db4e367
dmooney-cs <dougm@connectsecure.com>
2025-11-24 13:39:03 -0500

    Update CS-Toolbox-Launcher-DevTools.ps1

--- CS-Toolbox-Launcher-DevTools.ps1
+++ CS-Toolbox-Launcher-DevTools.ps1
@@ -15,7 +15,6 @@ param(
 # Config
 # --------------------------
 $ZipUrl       = 'https://betadevtools.myconnectsecure.com/agents/toolbox/toolbox.zip'            # RAW file url
-$HashUrl      = 'https://github.com/dmooney-cs/prod/raw/main/prod-01-01.zip.sha256'     # Optional: remote hash file (single line or "hash  filename")
 $ZipPath      = Join-Path $env:TEMP 'prod-01-01.zip'
 $ExtractPath  = 'C:\CS-Toolbox-TEMP'
 $DestRoot     = Join-Path $ExtractPath 'prod-01-01'
